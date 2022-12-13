e9b0e2caa3ba0825f437a0a88600ab9639930844
Miki <miki.lombi@gmail.com>
2022-12-13 01:08:56 +0100

    wip(frontend): add basic css style, color palettes, routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,30 @@
-import WrappedCard from '../components/WrappedCard';
+import Link from 'next/link';
+import { useState } from 'react';
 
-function App() {
+function Wrapped() {
+  const [username, setUsername] = useState('');
+
   return (
-    <div className="App">
-      <WrappedCard type="Hello SH!" score="10" img="wrapped3.png" />
-    </div>
+    <>
+      <div className='body-wrap'>
+        <div className='container'>
+          <div className='content-wrap'>
+            <div className='content-header'>
+              <h1>Open Source Wrapped</h1>
+            </div>
+            <div className='content-body'>
+              <input type={'text'} className='username' placeholder='Your username' onChange={(e) => setUsername(e.target.value)} />
+            </div>
+            <div className='content-footer'>
+              <Link href={`/wrapped/${username}`}>
+                <button disabled={username.length < 1} className='button'>Wrap me up</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
-export default App;
+export default Wrapped;
