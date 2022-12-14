3e6df265fd31c044359eecc30c40ace11b43ae7d
404answernotfound <404answernotfound.public@gmail.com>
2022-12-14 17:04:34 +0100

    format/new formatted files added to repo

--- src/components/WrappedCards.tsx
+++ src/components/WrappedCards.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import { renderToString } from 'react-dom/server';
+import { renderToString } from 'react-dom/server'
 import { useGetGithubInfos } from '../hooks/useGithubInfos';
 import { TWrappedCard } from '../types/TWrappedCard';
 import {
--- src/pages/wrapped/[username].tsx
+++ src/pages/wrapped/[username].tsx
@@ -1,6 +1,5 @@
 import WrappedCard from '@/components/WrappedCard';
 import { useSession } from 'next-auth/react';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import Image from 'next/image';
