170abf8aa4a80afa212a82edd716ffec23bb28b0
Miki <miki.lombi@gmail.com>
2022-12-14 19:22:20 +0100

    fix: og:image url

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head"
+import { useRouter } from "next/router";
 
 export default function Layout({ children }: any) {
+    const router = useRouter();
+    const { user } = router.query || '';
     return (
         <>
             <Head>
@@ -8,7 +11,7 @@ export default function Layout({ children }: any) {
             <meta property="og:type" content="website" />
             <meta property="og:title" content="Github Wrapped 2022 - By Schrodinger Hat" />
             <meta property="og:description" content="Github Wrapped 2022 - By Schrodinger Hat is the wrapped of the year for your open source contributions" />
-            <meta property="og:image" content="https://wrapped.schrodinger-hat.it/.netlify/functions/generator" />
+            <meta property="og:image" content={`https://wrapped.schrodinger-hat.it/.netlify/functions/generator?username=${user}`} />
 
 
             <meta name="twitter:card" content="summary_large_image" />
@@ -16,7 +19,7 @@ export default function Layout({ children }: any) {
             <meta property="twitter:url" content="https://wrapped.schrodinger-hat.it" />
             <meta name="twitter:title" content="Github Wrapped 2022 - By Schrodinger Hat" />
             <meta name="twitter:description" content="Github Wrapped 2022 - By Schrodinger Hat is the wrapped of the year for your open source contributions" />
-            <meta name="twitter:image" content="https://wrapped.schrodinger-hat.it/.netlify/functions/generator" />
+            <meta name="twitter:image" content={`https://wrapped.schrodinger-hat.it/.netlify/functions/generator?username=${user}`} />
 
             </Head>
             <main>{children}</main>
