d72f9614a1ccb8de90074497963e352756ec8be5
Miki <miki.lombi@gmail.com>
2022-12-14 18:50:08 +0100

    feat: add downloadble image

---
+++ src/utils/dataUrlToBlob.ts
@@ -0,0 +1,23 @@
+const dataURLtoBlob = (dataURI: string) => {
+  const byteString = atob(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+  // write the bytes of the string to an ArrayBuffer
+  const ab = new ArrayBuffer(byteString.length);
+
+  // create a view into the buffer
+  const ia = new Uint8Array(ab);
+
+  // set the bytes of the buffer to the correct values
+  for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+  // write the ArrayBuffer to a blob, and you're done
+  var blob = new Blob([ab], {type: mimeString});
+  return blob;
+}
+
+export default dataURLtoBlob;
---
+++ src/utils/svgToPng.ts
@@ -0,0 +1,19 @@
+import dataURLtoBlob from "./dataUrlToBlob";
+
+const svgString2Image = (svgString: string, width: number, height: number, format: string = 'png', callback: Function) => {
+    const svgData = svgString;
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    canvas.width = width;
+    canvas.height = height;
+    const image = new Image();
+    image.onload = function () {
+        context.clearRect(0, 0, width, height);
+        context.drawImage(image, 0, 0, width, height);
+        var pngData = canvas.toDataURL('image/' + format);
+        callback(dataURLtoBlob(pngData));
+    };
+    image.src = svgData;
+}
+
+export default svgString2Image;
