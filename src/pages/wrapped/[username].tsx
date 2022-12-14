144a251c4561226c9cc497dd53642cc6d6330474
Miki <miki.lombi@gmail.com>
2022-12-14 01:50:01 +0100

    wip: add image generation display page

--- src/pages/wrapped/[username].tsx
+++ src/pages/wrapped/[username].tsx
@@ -3,6 +3,7 @@ import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import Image from 'next/image';
 
 function Wrapped() {
   const router = useRouter();
@@ -24,6 +25,13 @@ function Wrapped() {
         img="/wrapped3.png"
         session={session}
       />
+      <div className="wrapped__wrap">
+        <Image alt="test" width={400} height={800} src={'/svg/panel-1.svg'} />
+        <Image alt="test" width={400} height={800} src={'/svg/panel-2.svg'} />
+        <Image alt="test" width={400} height={800} src={'/svg/panel-4.svg'} />
+        <Image alt="test" width={400} height={800} src={'/svg/panel-5.svg'} />
+        <Image alt="test" width={400} height={800} src={'/svg/panel-6.svg'} />
+      </div>
     </div>
   );
 }
