83861eb7b5ee059a55667254aad070e153428bd3
Miki <miki.lombi@gmail.com>
2022-12-14 17:01:01 +0100

    feat: svg as react component

---
+++ src/constants/slide-templates.ts
@@ -0,0 +1,16 @@
+import {
+    RecapNumStatsCard,
+    RecapNumTxtCard,
+    RecapTopCard,
+    TopCard,
+    WelcomeCard,
+} from "@/components/Cards";
+
+
+export const PANEL_TPL = {
+    PANEL_WELCOME: WelcomeCard,
+    PANEL_RECAP_NUM_TXT: RecapNumTxtCard,
+    PANEL_RECAP_NUM_STATS: RecapNumStatsCard,
+    PANEL_TOP_X: TopCard,
+    PANEL_RECAP_TOP: RecapTopCard,
+};
---
+++ src/pages/wrapped/[user].tsx
@@ -0,0 +1,16 @@
+import WrappedCards from "@/components/WrappedCards";
+import { useSession } from 'next-auth/react'
+
+function Wrapped() {
+  const { data: session } = useSession();
+
+  return (
+    <div className="Wrapped">
+      <div className="wrapped__wrap">
+        <WrappedCards img='' type="Hello SH!" score="10" session={session}/>
+      </div>
+    </div>
+  );
+}
+
+export default Wrapped;
