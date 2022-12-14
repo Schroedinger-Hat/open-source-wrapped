42e5e9a077cac9a3bf43d65332659212c0dd0274
Miki <miki.lombi@gmail.com>
2022-12-14 01:50:01 +0100

    wip: add image generation display page

--- src/pages/wrapped/user.tsx
+++ src/pages/wrapped/user.tsx
@@ -1,5 +1,6 @@
 import WrappedCard from "@/components/WrappedCard";
 import { useSession } from 'next-auth/react'
+import Image from "next/image";
 
 
 function Wrapped() {
@@ -8,6 +9,13 @@ function Wrapped() {
   return (
     <div className="Wrapped">
       <WrappedCard type="Hello SH!" score="10" img="/wrapped3.png"  session={session}/>
+      <div className="wrapped__wrap">
+        <Image alt='test' width={400} height={800} src={'/svg/panel-1.svg'} />
+        <Image alt='test' width={400} height={800} src={'/svg/panel-2.svg'} />
+        <Image alt='test' width={400} height={800} src={'/svg/panel-4.svg'} />
+        <Image alt='test' width={400} height={800} src={'/svg/panel-5.svg'} />
+        <Image alt='test' width={400} height={800} src={'/svg/panel-6.svg'} />
+      </div>
     </div>
   );
 }
