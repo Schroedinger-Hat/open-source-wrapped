e72601721ca3f4c2c7639f0ec21b67d20fc60e30
nicpuppa <puppanicola@gmail.com>
2022-12-18 09:08:13 +0100

    feat: handle error if user has only private infos

--- src/utils/github.tsx
+++ src/utils/github.tsx
@@ -21,8 +21,14 @@ export function getTopLanguage(githubInfos: TGitHubUser): string {
         acc[val] = (acc[val] || 0) + 1;
         return acc;
       }, {});
+
+  const languages: string [] = Object.keys(languageMap)
+
+  if (languages.length === 0) {
+    return '';
+  }
   // @ts-ignore
-  return Object.keys(languageMap).reduce((a, b) =>
+  return languages.reduce((a, b) =>
     // @ts-ignore
     languageMap[a] > languageMap[b] ? a : b
   );
